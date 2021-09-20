1fa04a3c1c1a00b186b8e89db67ba031fadb3e18
sars723 <sarasalomonn@gmail.com>
2021-09-20 15:27:44 +0200

    commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import JobList from "./JobList";
+import { connect } from "react-redux";
+import { fillJobsAction } from "../actions";
+import { Alert, Spinner } from "react-bootstrap";
+const mapStateToProps = (state) => state;
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchJobs: () => dispatch(fillJobsAction()),
+});
+const Home = (props) => {
+  /* const [jobs, setJobs] = useState([]); */
+  /* const fetchJobs = async () => {
+    console.log("e" + props.searchQuery);
+    if (props.searchQuery) {
+      try {
+        const response = await fetch(
+          "https://strive-jobs-api.herokuapp.com/jobs?title=" +
+            props.searchQuery
+        );
+        if (response.ok) {
+          const fetchedJobs = await response.json();
+          console.log(fetchedJobs.data);
+          setJobs(fetchedJobs.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const fetchWithCategory = async () => {
+    console.log("c" + props.searchCategory);
+    if (props.searchCategory) {
+      try {
+        const response = await fetch(
+          "https://strive-jobs-api.herokuapp.com/jobs?category=" +
+            props.searchCategory
+        );
+        if (response.ok) {
+          const fetchedJobs = await response.json();
+          console.log("fetchedCategory=", fetchedJobs.data);
+          setJobs(fetchedJobs.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchWithCategory();
+    fetchJobs();
+  }, [props.searchQuery, props.searchCategory]);
+ */
+  useEffect(() => {
+    props.fetchJobs();
+  }, []);
+  return (
+    <div className="mt-5">
+      {props.job.error ? (
+        <Alert variant="danger">SOMETHING WENT WRONG!</Alert>
+      ) : props.job.loading ? (
+        <Spinner animation="border" variant="success" />
+      ) : (
+        <>
+          {console.log("jobs from state", props.job.jobs)}
+          {props.job.jobs.map((job) => (
+            <JobList key={job._id} jobb={job} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useState } from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { withRouter, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { fillJobsAction } from "../actions";
+
+const mapStateToProps = (state) => state;
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchJobsWithSearch: (search) => dispatch(fillJobsAction(search)),
+});
+const MyNavbar = ({ fetchJobsWithSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  /*  const [searchCategory, setSearchCategory] = useState(""); */
+
+  /*   const handleSubmit = (e) => {
+    e.preventDefault();
+    props.setSearchCategory(searchCategory);
+  }; */
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Job Search Engine</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        {/* <p onClick={() => props.history.push("/favourites", props.job)}>
+          {props.job.company_name}
+        </p> */}
+        <Link className="nav-link" to="/favourites">
+          Favourite
+        </Link>
+      </Nav>
+      <Form inline>
+        <FormControl
+          type="text"
+          placeholder="Search"
+          className="mr-sm-2"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <Button
+          variant="outline-info"
+          onClick={fetchJobsWithSearch(searchQuery)}
+        >
+          Search with title
+        </Button>
+      </Form>
+      {/*  <Form inline className="ml-5">
+        <FormControl
+          type="text"
+          placeholder="Search"
+          className="mr-sm-2"
+          value={searchCategory}
+          onChange={(e) => setSearchCategory(e.target.value)}
+        />
+        <Button
+          variant="outline-info"
+          onClick={fetchJobsWithSearch(searchCategory)}
+        >
+          Search with Category
+        </Button>
+      </Form> */}
+    </Navbar>
+  );
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(MyNavbar));
